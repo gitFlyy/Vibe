27937b05fa0696de54043e63d23e979badc66981
Humayun <humayunbilal04@gmail.com>
2025-08-06 14:15:09 +0500

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event}) => {
-    // Create a new agent with a system prompt (you can add optional tools, too)
+  async ({ event, step}) => {
+    const sandboxId  = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-2222");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
@@ -15,8 +21,14 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet: ${event.data.value}`,
     );
-    console.log(output);
-    
-    return { output };
+    //console.log(output);
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 );
