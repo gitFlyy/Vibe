580c77847b896a966025414be27b451f1e9728a2
Humayun <humayunbilal04@gmail.com>
2025-08-10 10:14:49 +0500

    10: messages ui

---
+++ src/modules/projects/ui/views/project-view.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useTRPC } from '@/trpc/client';
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { MessagesContainer } from  "../components/messages-container"
+import { Suspense } from "react";
+
+interface Props {
+    projectId: string;
+};
+
+export const ProjectView = ({ projectId }: Props) => {
+    const trpc = useTRPC();
+    return (
+        <div className="h-screen">
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel
+                defaultSize={35}
+                minSize={20}
+                className="flex flex-col min-h-0"
+                >
+                    <Suspense fallback={<div>Loading messages...</div>}>
+                        <MessagesContainer projectId={projectId} />
+                    </Suspense>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel
+                defaultSize={65}
+                minSize={50}
+                >
+                   todo PREVIEW
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </div>
+    );
+
+};
